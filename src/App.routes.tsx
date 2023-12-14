274a806a197da539766f3cc95b746e7cb58b822f
Mykhailo Yanitskyi <mykhailoyanitskyi@nerdzlab.com>
2023-12-14 15:41:43 +0200

    add google form

--- src/App.routes.tsx
+++ src/App.routes.tsx
@@ -1,18 +1,23 @@
 import App from "./App.tsx";
-import {Olenka} from "./Olenka.tsx";
-
+import { Olenka } from "./Olenka.tsx";
+import FormPage from "./FormPage.tsx";
 
 export enum APP_LINKS {
-    HOME = "/olenka",
-    OLENKA = "/olenka/reveal",
+  HOME = "/olenka",
+  OLENKA = "/olenka/reveal",
+  FORM = "/olenka/form",
 }
 
 export const APP_ROUTES = [
-    {
-        path: APP_LINKS.HOME,
-        component: App,
-    },  {
-        path: APP_LINKS.OLENKA,
-        component: Olenka,
-    },
-]
+  {
+    path: APP_LINKS.HOME,
+    component: App,
+  },
+  {
+    path: APP_LINKS.OLENKA,
+    component: Olenka,
+  },  {
+    path: APP_LINKS.FORM,
+    component: FormPage,
+  },
+];
